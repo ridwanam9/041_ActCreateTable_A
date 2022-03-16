86140b2f587164284608393731d6ed8e313d3f54
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2022-03-16 14:30:52 +0700

    Add project files.

---
+++ create_table/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace create_table
+{
+    class Program
+    {
+
+        public void CreateTable() 
+        { 
+            
+        }
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
