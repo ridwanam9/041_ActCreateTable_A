36b967d69ee78ec7b8ce8d86502ae842fc23bef6
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2022-03-16 14:44:22 +0700

    membuat method CreateTable

--- create_table/Program.cs
+++ create_table/Program.cs
@@ -11,13 +11,31 @@ namespace create_table
     {
 
         public void CreateTable() 
-        { 
-            
+        {
+            SqlConnection con = null;
+            try {
+                con = new SqlConnection("data source=DESKTOP-KVHUS77\\RIDWANAM;database=ProdiTI;Integrated Security = True");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Coba (NIM char(12) not null primary key, " +
+                    "Nama varchar(50), Alamat varchar(255), Jenis_kelamin(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel suskes dibuat");
+                Console.ReadKey();
+            }
+            catch (Exception e) {
+                Console.WriteLine("Oops, sepertinya ada yang salah." + e);
+                Console.ReadKey();
+            }
+            finally {
+                con.Close();
+            }
         }
         static void Main(string[] args)
         {
             
-
+            
         }
     }
 }
